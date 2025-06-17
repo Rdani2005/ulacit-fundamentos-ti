a1657255d517464de7c52901ad0ff68c519f852d
Rdani2005 <daniel.sequeira@betanysports.eu>
2025-06-16 20:22:12 -0600

    feat: adding initial code for python

---
+++ HolaMundo/main.py
@@ -0,0 +1,38 @@
+# This is a Python Comment
+# Demo01 - first Hello World Program
+# Author: Danny Sequeira
+# Date: 2025-06-16
+
+
+print("Hello World!")  # This prints Hello World to the console
+
+# VARIABLES - keep information in memory
+# rules for variable names:
+# 1. Must start with a letter or underscore
+# 2. Can contain letters, numbers, and underscores
+# 3. Cannot use reserved keywords (like 'if', 'else', 'while', etc.)
+
+
+# ::::::::::::::: Convention ::::::::::::::::
+# 4. Use descriptive names (e.g., full_name, age, is_active)
+# 5. Use snake_case for variable names (e.g., full_name, user_age)
+# 6. Use ALL_CAPS for constants (e.g., MAX_VALUE, PI)
+
+full_name = "Danny Sequeira"
+print(full_name)
+age = 30
+
+print(":::::::::::::::::::::::::::::::")
+
+client_name = "John Doe"
+client_age = 25
+is_active = True
+client_address = "123 Main St"
+client_phone = "555-1234"
+
+
+print(f"Client Name: {client_name}")
+print(f"Client Age: {client_age}")
+print(f"Is Active: {is_active}")
+print(f"Client Address: {client_address}")
+print(f"Client Phone: {client_phone}")
