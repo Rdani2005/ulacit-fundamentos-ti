3cd003f997175ec6cdba652e6128041c7a8076ed
Rdani2005 <daniel.sequeira@betanysports.eu>
2025-06-23 20:52:17 -0600

    feat: adding email string conversor

--- demo-2/main.py
+++ demo-2/main.py
@@ -4,14 +4,28 @@
 # print the new email.
 
 
+# name = input("cual es tu nombre?: ")
+# firstLastName = input("cual es tu primer apellido?: ")
+# secondLastName = input("cual es tu segundo apellido?: ")
+# domain = input("What is the domain for your email?: ")
+#
+# name = name.replace(" ", ".")
+# firstLastName = firstLastName.replace(" ", ".")
+# secondLastName = secondLastName.replace(" ", ".")
+#
+#
+# print(f"Tu email es: {name.lower()}.{firstLastName.lower()}.{secondLastName.lower()}@{domain.lower()}")
+
+
+
+
 name = input("cual es tu nombre?: ")
-firstLastName = input("cual es tu primer apellido?: ")
-secondLastName = input("cual es tu segundo apellido?: ")
+lastName = input("cual es tu apellido?: ")
 domain = input("What is the domain for your email?: ")
 
 name = name.replace(" ", ".")
-firstLastName = firstLastName.replace(" ", ".")
-secondLastName = secondLastName.replace(" ", ".")
+lastName = lastName.replace(" ", ".")
 
 
-print(f"Tu email es: {name.lower()}.{firstLastName.lower()}.{secondLastName.lower()}@{domain.lower()}")
+print(f"Tu email es: {name.lower()[:2]}{lastName.lower()[:4]}@{domain.lower()}")
+
